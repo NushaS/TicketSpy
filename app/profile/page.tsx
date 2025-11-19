c494163b8da71c5519e535aefc47b83b8740a6e7
ayagriswold <aya@sharperinfo.com>
2025-11-19 02:55:39 -0800

    Create and style profile settings page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,35 +1,45 @@
-import { redirect } from 'next/navigation';
+import ProfileInfo from '@/components/profile/ProfileInfo';
+import NotificationSettings from '@/components/profile/NotificationSettings';
+import { ArrowLeft, Check } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../logo.png';
+import styles from './profile.module.css';
 
-import { createClient } from '@/lib/supabase/server';
-import { InfoIcon } from 'lucide-react';
-import { FetchDataSteps } from '@/components/tutorial/fetch-data-steps';
-
-export default async function ProfilePage() {
-  const supabase = await createClient();
-
-  const { data, error } = await supabase.auth.getClaims();
-  if (error || !data?.claims) {
-    redirect('/auth/login');
-  }
-
+// page.tsx
+export default function ProfilePage() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated user
-        </div>
+    <>
+      <header className={styles.profileHeader}>
+        <Link href="/" className={styles.backButton}>
+          <ArrowLeft className={styles.arrowLeft} />
+        </Link>
+        <h1 className={styles.headerTitle}>profile settings</h1>
+        {/* <div className={styles.logoContainer}>
+          <Image
+            src={logo}
+            alt="TicketSpy Logo"
+            width={150}
+            height={50}
+            priority
+            className={styles.logo}
+          />
+        </div> */}
+        <div>ticketspy</div>
+      </header>
+      <div className={styles.profilePage}>
+        <section className={styles.section}>
+          <h2 className={styles.sectionHeader}>notification settings</h2>
+          <NotificationSettings />
+        </section>
+
+        <section className={styles.section}>
+          <h2 className={styles.sectionHeader}>my account</h2>
+          <div className={styles.sectionContent}>
+            <ProfileInfo />
+          </div>
+        </section>
       </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(data.claims, null, 2)}
-        </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
-      </div>
-    </div>
+    </>
   );
 }
