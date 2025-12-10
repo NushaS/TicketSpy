cc4cc6af1bac53142ddde10454eb9a4a49053ab3
ayagriswold <aya@sharperinfo.com>
2025-12-09 21:46:14 -0800

    linter
    spacing adjustment
    
    update package-lock.json
    
    Utilize new bookmark color

--- components/map/BookmarkNameModal.tsx
+++ components/map/BookmarkNameModal.tsx
@@ -52,9 +52,11 @@ const BookmarkNameModal: React.FC<BookmarkNameModalProps> = ({
               required
             />
           </div>
-          <div className={styles.bookmarkSubmit}>
-            
-          </div>
+          <div className={styles.bookmarkSubmit}></div>
+          <button className={styles.submitBookmarkButton} disabled={isSubmitting} type="submit">
+            <FaCheck size={16} />
+            {isSubmitting ? 'saving...' : 'save bookmark'}
+          </button>
           <span className={styles.linkToProfileText}>
             To receive notifications for events near this spot, ensure that{' '}
             <a href="profile-settings/" className={styles.profileLink}>
@@ -62,10 +64,6 @@ const BookmarkNameModal: React.FC<BookmarkNameModalProps> = ({
             </a>{' '}
             .
           </span>
-          <button className={styles.submitBookmarkButton} disabled={isSubmitting} type="submit">
-            <FaCheck size={16} />
-            {isSubmitting ? 'saving...' : 'save bookmark'}
-          </button>
         </form>
       </div>
     </div>
